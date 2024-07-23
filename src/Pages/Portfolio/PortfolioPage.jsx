77988ff87cb049e075799d4048981f2f4b26a46e
BreTre <bctrepal@gmail.com>
2024-07-22 22:20:27 -0400

    built and styled portfolio page

--- src/Pages/Portfolio/PortfolioPage.jsx
+++ src/Pages/Portfolio/PortfolioPage.jsx
@@ -1,8 +1,48 @@
 import './portfolio-page.css';
-export default function AboutPage() {
+import Project from './Project';
+import nutrifitScreen from '../../Assets/images/nutrifitscreenshot.jpg';
+import nutrifitLogo from '../../Assets/logos/NutriFit-Logo-Combined.svg';
+import timecapsuleScreen from '../../Assets/images/timecapsulescreenshot.jpg';
+import timecapsuleLogo from '../../Assets/logos/TimeCapsuleFavicon.svg';
+import timtrepalScreen from '../../Assets/images/timtrepalscreenshot.jpg';
+import timtrepalLogo from '../../Assets/logos/TimTrepal.svg';
+
+const nutrifit = {
+    image: nutrifitScreen,
+    name: 'NutriFit',
+    link: 'https://nutrifit-47ii.onrender.com',
+    logo: nutrifitLogo,
+    github: 'https://github.com/rvanetta97/NutriFit',
+    techs: 'CSS, JavaScript, Express.js, Node.js, PostgreSQL, Handlebars.js, Charts.js, web APIs',
+};
+
+const timecapsule = {
+    image: timecapsuleScreen,
+    name: 'TimeCapsule',
+    link: 'https://beeceetee.github.io/TimeCapsule',
+    logo: timecapsuleLogo,
+    github: 'https://github.com/BeeCeeTee/TimeCapsule',
+    techs: 'HTML, CSS, Bulma, web APIs',
+};
+
+const timtrepal = {
+    image: timtrepalScreen,
+    name: 'Tim Trepal',
+    link: 'https://www.timtrepal.com',
+    logo: timtrepalLogo,
+    github: 'https://github.com/thomas-forte/tim-trepal',
+    techs: 'HTML, CSS, JavaScript',
+};
+
+export default function PortfolioPage() {
     return (
         <div className='portfolio-page'>
-            <h1>Portfolio</h1>
+            <h1 className='portfolio-heading'>Portfolio</h1>
+            <div className='portfolio-container'>
+                <Project image={nutrifit.image} name={nutrifit.name} link={nutrifit.link} logo={nutrifit.logo} github={nutrifit.github} techs={nutrifit.techs} />
+                <Project image={timecapsule.image} name={timecapsule.name} link={timecapsule.link} logo={timecapsule.logo} github={timecapsule.github} techs={timecapsule.techs} />
+                <Project image={timtrepal.image} name={timtrepal.name} link={timtrepal.link} logo={timtrepal.logo} github={timtrepal.github} techs={timtrepal.techs} />
+            </div>
         </div>
     );
 }
--- src/Pages/Portfolio/Project.jsx
+++ src/Pages/Portfolio/Project.jsx
@@ -1,1 +1,19 @@
-import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import './project.css';
+
+export default function Project(props) {
+    return (
+        <div className='project'>
+            <img className='project-image' src={props.image} alt={`Image of deployed ${props.name} site`} />
+            <div className='project-container'>
+                <h3 className='project-subtitle'>{props.name}</h3>
+                <div className='project-links'>
+                    <a href={props.link} target="_blank"><img className='project-logo' src={props.logo} alt={`${props.name} logo`} /></a>
+                    <a href={props.github} target="_blank"><FontAwesomeIcon icon={faGithub} className='project-github' /></a>
+                </div>
+                <p className='project-techs'>{props.techs}</p>
+            </div>
+        </div>
+    );
+}
