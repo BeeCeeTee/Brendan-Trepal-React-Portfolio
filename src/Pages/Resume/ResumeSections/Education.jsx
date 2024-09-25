762f00f8cf2658a6c87e3d82dacdf7d9fe7937c1
BreTre <bctrepal@gmail.com>
2024-09-25 19:26:24 -0400

    tweaked paragraph spacing on about page and finished optimizing resume page for tablet and mobile views

--- src/Pages/Resume/ResumeSections/Education.jsx
+++ src/Pages/Resume/ResumeSections/Education.jsx
@@ -7,7 +7,7 @@ export default function Education() {
             <div className='education-container'>
                 <div className='education-subtitle-container'>
                     <h4 className='education-subtitle'>Full Stack Web Development Boot Camp Certificate</h4>
-                    <p className='education-type-location'>Case Western Reserve University, Cleveland, OH</p>
+                    <p className='education-location'>Case Western Reserve University, Cleveland, OH</p>
                 </div>
                 <p className='education-paragraph'>An intensive, 6-month program focused on gaining technical programming skills in HTML5, CSS3, JavaScript,
                     JQuery, Bootstrap, Firebase, Node.js, MySQL, MongoDB, Express, Handelbars.js, and ReactJS.</p>
@@ -15,7 +15,7 @@ export default function Education() {
             <div className='education-container'>
                 <div className='education-subtitle-container'>
                     <h4 className='education-subtitle'>Bachelor of Science in Communication</h4>
-                    <p className='education-type-location'>Ohio University, Athens, OH</p>
+                    <p className='education-location'>Ohio University, Athens, OH</p>
                 </div>
                 <p className='education-paragraph'>A 4-year undergraduate degree specializing in visual communication.</p>
             </div>
